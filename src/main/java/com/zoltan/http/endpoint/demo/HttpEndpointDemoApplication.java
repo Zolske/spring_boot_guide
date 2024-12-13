fccd77a8712874e4678b5d77392b7d76071f9648
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2024-12-13 18:53:40 +0000

    initial commit

---
+++ src/main/java/com/zoltan/http/endpoint/demo/HttpEndpointDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zoltan.http.endpoint.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpEndpointDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpEndpointDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoltan/http/endpoint/demo/HttpEndpointDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoltan.http.endpoint.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpEndpointDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
